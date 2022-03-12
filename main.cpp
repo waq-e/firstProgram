1994aea3ce5d45a3315b265da37c8aac40d56087
waq-e <whomomlikes@hotmail.com>
2022-03-11 23:58:59 -0600

    C++ output and language basics.
    Smallest possible C++ program (not counting comments.  (-:

--- main.cpp
+++ main.cpp
@@ -15,7 +15,14 @@
  *			Parse data and process strings
  *		Define your own types with enum
  */
+// preprocessor directive to include the iostream library before compiling the program
+#include <iostream>
 
+// use the version of cout provided by the standard library
+using std::cout;
 
+// main function is called at the beginning of every c++ program, it will return an integer and take no arguments
 int main() {
+	// read right-to-left, Hello!\n is sent to cout (the output stream for the terminal)
+	cout << "Hello!" << "\n";
 }
