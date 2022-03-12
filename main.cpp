61aa1a2523e185dc0c5d3c54ec9097db6470afba
waq-e <whomomlikes@hotmail.com>
2022-03-11 23:40:28 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+/**
+ * Goals:
+ * 		Write code with correct syntax
+ * 		Write and run first c++ program
+ * 		Learn about variables and containers
+ * 			Variable types
+ * 			Vectors
+ * 			Using auto
+ * 		Functions and control structures
+ * 			For loops
+ * 			Functions
+ * 			If statements and while loops
+ * 		Data input
+ * 			Read data from a file
+ *			Parse data and process strings
+ *		Define your own types with enum
+ */
+
+
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
